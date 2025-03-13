3729c8263e76d3326c48da56dd46fa9dbd3f5aa8
McKenna Brown <153936426+mckennabrown0224@users.noreply.github.com>
2025-03-13 12:30:25 -0600

    chatbot working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { InputDesign } from "./components/index.js/InputDesign";
+import ChatbotPage from "./components/chatbot/ChatInterface";
+import { ChatButton } from "./components/ChatButton"; // Import the ChatButton
 
 function App() {
   return (
-      <InputDesign />
+    <Router>
+      {/* ChatButton is placed outside of Routes so it appears on every page */}
+      <ChatButton />  
+      <Routes>
+        <Route path="/" element={<InputDesign />} />
+        <Route path="/chatbot" element={<ChatbotPage />} />
+      </Routes>
+    </Router>
   );
 }
 
