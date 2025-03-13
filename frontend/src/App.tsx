a839b82d51c06cf3047cd39b51120e96dcac4108
Sp1derAp0calypse <131923585+Sp1derAp0calypse@users.noreply.github.com>
2025-03-13 03:38:58 -0600

    countdowntimer component
    well uh. in theory it works ig; i'll have to play with it more when the full infrastructure is in place.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CountdownTimer from "./components\\index.js/CountdownTimer.tsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <h1>Days Until Temple</h1>
+        <CountdownTimer />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
