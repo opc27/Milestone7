f7fa0bcf81c5070eb3c867992246c70e09e7024a
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-03-26 12:22:33 -0600

    Pick Up button sends you to Modules List page (temporary fix until modules are connected to users in database)

--- backend/Milestone7_backend/Milestone7_backend/obj/Debug/net8.0/Milestone7_backend.AssemblyInfo.cs
+++ backend/Milestone7_backend/Milestone7_backend/obj/Debug/net8.0/Milestone7_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milestone7_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f2613866dba7cc8e0ee28afe5cb7fbd436cd01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b59218e0a0a211eba6e6f68089eb8c27437bf1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milestone7_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milestone7_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
